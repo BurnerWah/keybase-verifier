fc0555f9efb55b476309df99e9e3c5b87a37ec6e
Burner <burner@burner.gay>
2022-07-04 13:21:56 -0500

    Removed unnecessary fetch keyword from index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,7 @@ export interface Env {
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
+  fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     return app.fetch(request, env, ctx)
   },
 }
