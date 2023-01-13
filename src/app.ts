dcc7acfebc6b38bfeb1556fd4bf00225f2588793
Burner <burner@burner.gay>
2023-01-12 19:09:14 -0600

    Joke

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,6 @@ app.get(
   '*',
   cache({
     cacheName: 'keybase',
-    // In 2077 what makes someone a criminal?
-    //
-    // Getting cock *loud edm music begins playing*
-    cacheControl: 'public, max-age=1735668000, s-maxage=1735668000',
   }),
 )
 
@@ -20,7 +16,17 @@ app.get('/.well-known/keybase.txt', async (ctx) => {
   const { host } = ctx.req.header()
   const attestation = await ctx.env.ATTESTATIONS.get(`${host}.txt`)
   if (attestation) {
-    return ctx.body(attestation.body, 200)
+    // In 2077 what makes someone a criminal?
+    //
+    // Getting cock *loud edm music begins playing*
+    const cacheSec =
+      2077 > new Date().getFullYear()
+        ? Math.ceil((new Date('Jan 2 2077') - new Date()) / 1000)
+        : 31536000
+
+    return ctx.body(attestation.body, 200, {
+      'Cache-Control': `public, max-age=${cacheSec}, s-maxage=${cacheSec}`,
+    })
   } else {
     return ctx.notFound()
   }
